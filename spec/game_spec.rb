f35f2571efcb38cc17daecaa5f981cdb3c1e8b32
Peter Miklos <pt.miklos@gmail.com>
2016-10-18 14:49:31 +0100

    players added to the game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,21 @@ require 'game'
 
 describe Game do
 
-  subject(:game) {described_class.new}
+  let(:player_1) {double(:player_1, name: "John")}
+  let(:player_2) {double(:player_2, name: "Jim")}
+  subject(:game) {described_class.new(player_1, player_2)}
 
+  context "#player_1" do
+    it "returns the first player" do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  context "#player_2" do
+    it "returns the second player" do
+      expect(game.player_2).to eq player_2
+    end
+  end
 
 
 end
